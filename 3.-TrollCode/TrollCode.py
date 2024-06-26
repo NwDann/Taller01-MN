3c57112073f749e3a9eba936fcce11c5676bd2c8
Danny I <118556155+NwDann@users.noreply.github.com>
2024-06-26 17:03:23 -0500

    Troll code acabado al 100

--- 3.-TrollCode/TrollCode.py
+++ 3.-TrollCode/TrollCode.py
@@ -1,12 +1,21 @@
 
-def guessing():
-    global sequence, prev_correctBits
+def guessing(N):
+    global sequence
+    outPut = "Q " + " ".join(map(str, sequence))
+    print(outPut)
+    prev_correctBits = int(input())
+    sys.stdout.flush()
+    
+    if prev_correctBits < N/2:
+        sequence = [1] * N
+        prev_correctBits = N - prev_correctBits
+    
     for i in range(N):
         sequence[i] = 1 - sequence[i]  # Changing from 0 to 1 and from 1 to 0
         outPut = "Q " + " ".join(map(str, sequence))
         print(outPut)
-        sys.stdout.flush()
         correctBits = int(input())
+        sys.stdout.flush()
         
         if correctBits == N:
             return True  # Correct sequence
@@ -16,16 +25,19 @@ def guessing():
             
         else:
             sequence[i] = 1 - sequence[i]  # Reversing the change
+            if (i == N - 1) and (correctBits == N - 1):
+                return True
             
     return False
+
+# ------------------------------MAIN---------------------------------------
 
 import sys
 
 N = int(input())
 sequence = [0] * N
-prev_correctBits = 0
 
-flag = guessing()
+flag = guessing(N)
 
 if flag:
     outPut = "A " + " ".join(map(str, sequence))
